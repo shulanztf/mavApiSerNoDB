b1d6e27399e7380e4eab8775d34dfb7a7a49aa3a
1426763507 <1426763507@qq.com>
2017-05-31 16:43:58 +0800

    java 动态代理 补充

--- src/main/java/com/tuil/HttpUtil.java
+++ src/main/java/com/tuil/HttpUtil.java
@@ -13,8 +13,16 @@ package com.tuil;
  * @version 1.0
  * 
  */
-
 public class HttpUtil {
 
-	
+	// 定义一个本地方法
+	public native void sayHello();
+
+	public static void main(String[] args) {
+		// 调用动态链接库
+		System.loadLibrary("HttpUtil");
+		HttpUtil jniDemo = new HttpUtil();
+		jniDemo.sayHello();
+	}
+
 }
