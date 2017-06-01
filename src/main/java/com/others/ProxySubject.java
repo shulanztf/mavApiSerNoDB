b13e372bd05da44754c73a2b6e42d7a914d8f187
1426763507 <1426763507@qq.com>
2017-06-01 13:37:08 +0800

    java动态代理 资料补充

---
+++ src/main/java/com/others/ProxySubject.java
@@ -0,0 +1,460 @@
+package com.others;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.security.AccessControlContext;
+import java.security.PrivilegedAction;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+
+import com.proxy.Person;
+
+/**
+ * 
+ * @Title: ProxySubject
+ * @Description:java动态代理 生成的class文件
+ * @Author: zhaotf
+ * @Since:2017年6月1日 下午1:35:30
+ * @Version:1.0
+ */
+@SuppressWarnings("serial")
+public final class ProxySubject extends Proxy implements Person {
+
+	// protected ProxySubject(InvocationHandler h) {
+	// super(h);
+	// // TODO Auto-generated constructor stub
+	// }
+
+	@Override
+	public void giveMoney() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static Method m17;
+	private static Method m8;
+	private static Method m18;
+	private static Method m21;
+	private static Method m5;
+	private static Method m15;
+	private static Method m7;
+	private static Method m9;
+	private static Method m16;
+	private static Method m2;
+	private static Method m1;
+	private static Method m10;
+	private static Method m4;
+	private static Method m11;
+	private static Method m19;
+	private static Method m3;
+	private static Method m0;
+	private static Method m13;
+	private static Method m20;
+	private static Method m6;
+	private static Method m12;
+	private static Method m14;
+
+	public ProxySubject(InvocationHandler paramInvocationHandler) {
+		super(paramInvocationHandler);
+	}
+
+	public final void wait(long paramLong) throws InterruptedException {
+		try {
+			this.h.invoke(this, m17, new Object[] { Long.valueOf(paramLong) });
+			return;
+		} catch (RuntimeException localRuntimeException) {
+			throw localRuntimeException;
+		} catch (Throwable localThrowable) {
+		}
+		throw new UndeclaredThrowableException(localThrowable);
+	}
+
+	public final Object doAs(Subject paramSubject, PrivilegedAction paramPrivilegedAction)
+	    throws 
+	  {
+	    try
+	    {
+	      return (Object)this.h.invoke(this, m8, new Object[] { paramSubject, paramPrivilegedAction });
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final void wait() throws InterruptedException {
+		try {
+			this.h.invoke(this, m18, null);
+			return;
+		} catch (RuntimeException localRuntimeException) {
+			throw localRuntimeException;
+		} catch (Throwable localThrowable) {
+		}
+		throw new UndeclaredThrowableException(localThrowable);
+	}
+
+	public final void notifyAll()
+	    throws 
+	  {
+	    try
+	    {
+	      this.h.invoke(this, m21, null);
+	      return;
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final void setReadOnly()
+	    throws 
+	  {
+	    try
+	    {
+	      this.h.invoke(this, m5, null);
+	      return;
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Subject getSubject(AccessControlContext paramAccessControlContext)
+	    throws 
+	  {
+	    try
+	    {
+	      return (Subject)this.h.invoke(this, m15, new Object[] { paramAccessControlContext });
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Object doAs(Subject paramSubject, PrivilegedExceptionAction paramPrivilegedExceptionAction)
+			throws PrivilegedActionException {
+		try {
+			return (Object) this.h.invoke(this, m7, new Object[] { paramSubject, paramPrivilegedExceptionAction });
+		} catch (RuntimeException localRuntimeException) {
+			throw localRuntimeException;
+		} catch (Throwable localThrowable) {
+		}
+		throw new UndeclaredThrowableException(localThrowable);
+	}
+
+	public final Object doAsPrivileged(Subject paramSubject, PrivilegedAction paramPrivilegedAction, AccessControlContext paramAccessControlContext)
+	    throws 
+	  {
+	    try
+	    {
+	      return (Object)this.h.invoke(this, m9, new Object[] { paramSubject, paramPrivilegedAction, paramAccessControlContext });
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final void wait(long paramLong, int paramInt) throws InterruptedException {
+		try {
+			this.h.invoke(this, m16, new Object[] { Long.valueOf(paramLong), Integer.valueOf(paramInt) });
+			return;
+		} catch (RuntimeException localRuntimeException) {
+			throw localRuntimeException;
+		} catch (Throwable localThrowable) {
+		}
+		throw new UndeclaredThrowableException(localThrowable);
+	}
+
+	public final String toString()
+	    throws 
+	  {
+	    try
+	    {
+	      return (String)this.h.invoke(this, m2, null);
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final boolean equals(Object paramObject)
+	    throws 
+	  {
+	    try
+	    {
+	      return ((Boolean)this.h.invoke(this, m1, new Object[] { paramObject })).booleanValue();
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Object doAsPrivileged(Subject paramSubject, PrivilegedExceptionAction paramPrivilegedExceptionAction,
+			AccessControlContext paramAccessControlContext) throws PrivilegedActionException {
+		try {
+			return (Object) this.h.invoke(this, m10,
+					new Object[] { paramSubject, paramPrivilegedExceptionAction, paramAccessControlContext });
+		} catch (RuntimeException localRuntimeException) {
+			throw localRuntimeException;
+		} catch (Throwable localThrowable) {
+		}
+		throw new UndeclaredThrowableException(localThrowable);
+	}
+
+	public final Set getPrincipals(Class paramClass)
+	    throws 
+	  {
+	    try
+	    {
+	      return (Set)this.h.invoke(this, m4, new Object[] { paramClass });
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Set getPrivateCredentials()
+	    throws 
+	  {
+	    try
+	    {
+	      return (Set)this.h.invoke(this, m11, null);
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Class getClass()
+	    throws 
+	  {
+	    try
+	    {
+	      return (Class)this.h.invoke(this, m19, null);
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Set getPrincipals()
+	    throws 
+	  {
+	    try
+	    {
+	      return (Set)this.h.invoke(this, m3, null);
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final int hashCode()
+	    throws 
+	  {
+	    try
+	    {
+	      return ((Integer)this.h.invoke(this, m0, null)).intValue();
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Set getPublicCredentials()
+	    throws 
+	  {
+	    try
+	    {
+	      return (Set)this.h.invoke(this, m13, null);
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final void notify()
+	    throws 
+	  {
+	    try
+	    {
+	      this.h.invoke(this, m20, null);
+	      return;
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final boolean isReadOnly()
+	    throws 
+	  {
+	    try
+	    {
+	      return ((Boolean)this.h.invoke(this, m6, null)).booleanValue();
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Set getPrivateCredentials(Class paramClass)
+	    throws 
+	  {
+	    try
+	    {
+	      return (Set)this.h.invoke(this, m12, new Object[] { paramClass });
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	public final Set getPublicCredentials(Class paramClass)
+	    throws 
+	  {
+	    try
+	    {
+	      return (Set)this.h.invoke(this, m14, new Object[] { paramClass });
+	    }
+	    catch (RuntimeException localRuntimeException)
+	    {
+	      throw localRuntimeException;
+	    }
+	    catch (Throwable localThrowable)
+	    {
+	    }
+	    throw new UndeclaredThrowableException(localThrowable);
+	  }
+
+	static {
+		try {
+			m17 = Class.forName("javax.security.auth.Subject").getMethod("wait", new Class[] { Long.TYPE });
+			m8 = Class.forName("javax.security.auth.Subject").getMethod("doAs", new Class[] {
+					Class.forName("javax.security.auth.Subject"), Class.forName("java.security.PrivilegedAction") });
+			m18 = Class.forName("javax.security.auth.Subject").getMethod("wait", new Class[0]);
+			m21 = Class.forName("javax.security.auth.Subject").getMethod("notifyAll", new Class[0]);
+			m5 = Class.forName("javax.security.auth.Subject").getMethod("setReadOnly", new Class[0]);
+			m15 = Class.forName("javax.security.auth.Subject").getMethod("getSubject",
+					new Class[] { Class.forName("java.security.AccessControlContext") });
+			m7 = Class.forName("javax.security.auth.Subject").getMethod("doAs",
+					new Class[] { Class.forName("javax.security.auth.Subject"),
+							Class.forName("java.security.PrivilegedExceptionAction") });
+			m9 = Class.forName("javax.security.auth.Subject").getMethod("doAsPrivileged",
+					new Class[] { Class.forName("javax.security.auth.Subject"),
+							Class.forName("java.security.PrivilegedAction"),
+							Class.forName("java.security.AccessControlContext") });
+			m16 = Class.forName("javax.security.auth.Subject").getMethod("wait",
+					new Class[] { Long.TYPE, Integer.TYPE });
+			m2 = Class.forName("java.lang.Object").getMethod("toString", new Class[0]);
+			m1 = Class.forName("java.lang.Object").getMethod("equals",
+					new Class[] { Class.forName("java.lang.Object") });
+			m10 = Class.forName("javax.security.auth.Subject").getMethod("doAsPrivileged",
+					new Class[] { Class.forName("javax.security.auth.Subject"),
+							Class.forName("java.security.PrivilegedExceptionAction"),
+							Class.forName("java.security.AccessControlContext") });
+			m4 = Class.forName("javax.security.auth.Subject").getMethod("getPrincipals",
+					new Class[] { Class.forName("java.lang.Class") });
+			m11 = Class.forName("javax.security.auth.Subject").getMethod("getPrivateCredentials", new Class[0]);
+			m19 = Class.forName("javax.security.auth.Subject").getMethod("getClass", new Class[0]);
+			m3 = Class.forName("javax.security.auth.Subject").getMethod("getPrincipals", new Class[0]);
+			m0 = Class.forName("java.lang.Object").getMethod("hashCode", new Class[0]);
+			m13 = Class.forName("javax.security.auth.Subject").getMethod("getPublicCredentials", new Class[0]);
+			m20 = Class.forName("javax.security.auth.Subject").getMethod("notify", new Class[0]);
+			m6 = Class.forName("javax.security.auth.Subject").getMethod("isReadOnly", new Class[0]);
+			m12 = Class.forName("javax.security.auth.Subject").getMethod("getPrivateCredentials",
+					new Class[] { Class.forName("java.lang.Class") });
+			m14 = Class.forName("javax.security.auth.Subject").getMethod("getPublicCredentials",
+					new Class[] { Class.forName("java.lang.Class") });
+			return;
+		} catch (NoSuchMethodException localNoSuchMethodException) {
+			throw new NoSuchMethodError(localNoSuchMethodException.getMessage());
+		} catch (ClassNotFoundException localClassNotFoundException) {
+		}
+		throw new NoClassDefFoundError(localClassNotFoundException.getMessage());
+	}
+}
--- src/main/java/com/proxy/DynamicProxyDemonstration.java
+++ src/main/java/com/proxy/DynamicProxyDemonstration.java
@@ -23,17 +23,17 @@ public class DynamicProxyDemonstration {
 	public static void main(String[] args) {
 		// 代理的真实对象
 		// Subject realSubject = new RealSubject();
-		Person realSubject = new Student("张三");
+		Person person = new Student("张三");
 
 		/**
 		 * InvocationHandlerImpl 实现了 InvocationHandler 接口，并能实现方法调用从代理类到委托类的分派转发
 		 * 其内部通常包含指向委托类实例的引用，用于真正执行分派转发过来的方法调用.
 		 * 即：要代理哪个真实对象，就将该对象传进去，最后是通过该真实对象来调用其方法
 		 */
-		InvocationHandler handler = new StuInvocationHandler(realSubject);
+		InvocationHandler handler = new StuInvocationHandler(person);
 
 		ClassLoader loader = handler.getClass().getClassLoader();
-		Class[] interfaces = realSubject.getClass().getInterfaces();
+		Class[] interfaces = person.getClass().getInterfaces();
 		/**
 		 * 该方法用于为指定类装载器、一组接口及调用处理器生成动态代理类实例
 		 */
@@ -41,7 +41,7 @@ public class DynamicProxyDemonstration {
 
 		System.out.println("动态代理对象的类型：" + subject.getClass().getName());
 
-		realSubject.giveMoney();
+		person.giveMoney();
 		// String hello = subject...SayHello("jiankunking");
 		// System.out.println(hello);
 		// 将生成的字节码保存到本地，
