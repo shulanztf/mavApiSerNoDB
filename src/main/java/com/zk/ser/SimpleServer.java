fd84d05e6acb39ae239f5e2ea9987b28bed6b6dc
1426763507 <1426763507@qq.com>
2017-09-07 16:49:01 +0800

    zookeeper原理机制 模拟

---
+++ src/main/java/com/zk/ser/SimpleServer.java
@@ -0,0 +1,52 @@
+package com.zk.ser;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 
+ * @Title: SimpleServer
+ * @Description:zookeeper学习
+ * @Author: zhaotf
+ * @Since:2017年9月6日 下午5:36:45
+ * @Version:1.0
+ */
+public class SimpleServer implements Runnable {
+
+	public static void main(String[] args) throws IOException {
+		int port = 18080;
+		SimpleServer server = new SimpleServer(port);
+		Thread thread = new Thread(server);
+		thread.start();
+	}
+
+	private int port;
+
+	public SimpleServer(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public void run() {
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+			Socket socket = null;
+			while (true) {
+				socket = server.accept();
+				new Thread(new SimpleServerHandler(socket)).start();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (server != null) {
+				try {
+					server.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+}
