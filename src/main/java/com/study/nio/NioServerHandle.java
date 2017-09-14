86e89592879ac90257c091c80f8c35b096b230ea
1426763507 <1426763507@qq.com>
2017-09-14 08:07:38 +0800

    JAVA NIO 开发

--- src/main/java/com/study/nio/NioServerHandle.java
+++ src/main/java/com/study/nio/NioServerHandle.java
@@ -59,19 +59,18 @@ public class NioServerHandle implements Runnable {
 
 	@Override
 	public void run() {
+		// 注册线程/事件轮询
+		SelectionKey key = null;
 		// 循环遍历selector
 		while (started) {
 			try {
-				// 无论是否有读写事件发生，selector每隔1s被唤醒一次
-				selector.select(1000);
-				// 阻塞,只有当至少一个注册的事件发生的时候才会继续.
-				// selector.select();
+				selector.select(1000); // 无论是否有读写事件发生，selector每隔1s被唤醒一次
+				// selector.select(); // 阻塞,只有当至少一个注册的事件发生的时候才会继续.
 				Set<SelectionKey> keys = selector.selectedKeys();
 				Iterator<SelectionKey> it = keys.iterator();
-				SelectionKey key = null;
 				while (it.hasNext()) {
 					key = it.next();
-					it.remove();
+					it.remove();// 移除已执行线程，及事件
 					try {
 						handleInput(key);
 					} catch (Exception e) {
@@ -96,6 +95,13 @@ public class NioServerHandle implements Runnable {
 			}
 	}
 
+	/**
+	 * 事件/线程状态转换，并，业务处理
+	 * 
+	 * @param key
+	 * @throws IOException
+	 *             void
+	 */
 	private void handleInput(SelectionKey key) throws IOException {
 		if (key.isValid()) {
 			// 处理新接入的请求消息
@@ -104,26 +110,20 @@ public class NioServerHandle implements Runnable {
 				// 通过ServerSocketChannel的accept创建SocketChannel实例
 				// 完成该操作意味着完成TCP三次握手，TCP物理链路正式建立
 				SocketChannel sc = ssc.accept();
-				// 设置为非阻塞的
-				sc.configureBlocking(false);
-				// 注册为读
-				sc.register(selector, SelectionKey.OP_READ);
+				sc.configureBlocking(false); // 设置为非阻塞的
+				sc.register(selector, SelectionKey.OP_READ); // 注册为读
 			}
-			// 读消息
+
+			// 读取消息
 			if (key.isReadable()) {
 				SocketChannel sc = (SocketChannel) key.channel();
-				// 创建ByteBuffer，并开辟一个1M的缓冲区
-				ByteBuffer buffer = ByteBuffer.allocate(1024);
-				// 读取请求码流，返回读取到的字节数
-				int readBytes = sc.read(buffer);
+				ByteBuffer bb = ByteBuffer.allocate(1024); // 创建ByteBuffer，并开辟一个1M的缓冲区
+				int readBytes = sc.read(bb); // 读取请求码流，返回读取到的字节数
 				// 读取到字节，对字节进行编解码
 				if (readBytes > 0) {
-					// 将缓冲区当前的limit设置为position=0，用于后续对缓冲区的读取操作
-					buffer.flip();
-					// 根据缓冲区可读字节数创建字节数组
-					byte[] bytes = new byte[buffer.remaining()];
-					// 将缓冲区可读字节数组复制到新建的数组中
-					buffer.get(bytes);
+					bb.flip(); // 将缓冲区当前的limit设置为position=0，用于后续对缓冲区的读取操作
+					byte[] bytes = new byte[bb.remaining()]; // 根据缓冲区可读字节数创建字节数组
+					bb.get(bytes); // 将缓冲区可读字节数组复制到新建的数组中
 					String expression = new String(bytes, "UTF-8");
 					System.out.println("服务器收到消息：" + expression);
 					// 处理数据
@@ -135,9 +135,9 @@ public class NioServerHandle implements Runnable {
 					}
 					// 发送应答消息
 					doWrite(sc, result);
+				} else {
+					// 没有读取到字节 忽略
 				}
-				// 没有读取到字节 忽略
-				// else if(readBytes==0);
 				// 链路已经关闭，释放资源
 				else if (readBytes < 0) {
 					key.cancel();
